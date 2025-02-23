01b3b6472d38e2edd07da6d85e209ff98fb1b34a
yshak <Vyshakn29@gmail.com>
2025-02-23 11:46:45 +0530

    updated profile

--- client/components/auth/Yourprofile.tsx
+++ client/components/auth/Yourprofile.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { Button } from '../ui/button'
 import { useRef,useState } from 'react'
 import { supabase, avatarsPublicurl } from "../../lib/supabase/supabaseClient"
-
+import { useUserStore } from '@/lib/stateStore'
 
 type Profile = {
     id: string,
@@ -14,7 +14,14 @@ type Profile = {
 
 function Yourprofile() {
     const uploadImgref = useRef<HTMLInputElement>(null);
+    const { userData } = useUserStore();
     const [Profile, setProfile] = useState<Profile>({ id: 'Avatar2.svg', url: "https://ppppwffeiuaabvrukckb.supabase.co/storage/v1/object/public/appAvatars/Avatar2.svg" })
+
+    const selectImage = () => {
+        uploadImgref.current?.click();
+        console.log('clicked')
+    };
+
     const uploadImg = async () => {
         const files = uploadImgref.current?.files;
         if (!files) {
@@ -44,15 +51,25 @@ function Yourprofile() {
         uploadImgref.current.value = null;
     }
 
-  return (
-    <motion.div initial={{opacity:50,x:0}} animate={{ opacity: 1,x:30 }} transition={{ duration: 1 }} className='w-1/2 flex justify-start items-start flex-col gap-y-6'>
-        <span className='font-extrabold text-2xl text-neutral-700'>Time to create Profile!</span>
-        <Image alt='avatar' src={Profile.url} />
-        <Button>Upload Profile</Button>
-        <input  onChange={uploadImg} ref={uploadImgref} className='hidden' type='file' />
-        <span>or</span>
-        <span>i will do it later</span>
-     </motion.div>
+    return (
+        <motion.div initial={{ opacity: 50, x: 0 }} animate={{ opacity: 1, x: 30 }} transition={{ duration: 1 }} className='w-1/2 flex justify-center items-center flex-col gap-y-6'>
+            <div className='flex justify-center items-center flex-col gap-y-6'>
+                <span className='font-extrabold text-2xl w-fit text-neutral-700'>Time to create Profile!</span>
+                <div className='w-full flex-col flexCenter gap-y-3'>
+                    <div className='w-fit flexCenter flex-col uppercase font-medium gap-y-2'>
+                    <Image className='rounded-full' width={100} height={100} alt='avatar' src={Profile.url} />
+                    <span className='text-neutral-500 text-xs'>{userData.fullname}</span>
+                    </div>
+                    <Button className='w-full' onClick={selectImage}>Upload Profile</Button>
+                    <input onChange={uploadImg} ref={uploadImgref} className='hidden' type='file' />
+                </div>
+            </div>
+            <div className='w-full bg-neutral-200 h-[1px]'></div>
+            <div className='flexCenter w-fit gap-y-1 flex-col'>
+                <span className='text-neutral-600 text-xs font-normal'>OR</span>
+                <span className='text-blue-600'>I will do it later</span>
+            </div>
+        </motion.div>
   )
 }
 
