eef3656303eaee95bcf681c4aeb06e6856dc43b6
yshak <Vyshakn29@gmail.com>
2025-02-27 14:14:31 +0530

    added the file upload

--- client/components/sellAuction/VehicleImg.tsx
+++ client/components/sellAuction/VehicleImg.tsx
@@ -1,40 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
 import useSellCount from '@/lib/stateStore/sellCount'
 import { VehicleIcon } from '@/public'
+import { useRef } from 'react'
 
 function VehicleImg() {
+  const [photos,setphotos] = useState<any[]>([]);
+  const photosRef = useRef<HTMLInputElement>(null)
+  console.log(photos)
+
+  const uploadPhotos = ()=>{
+    photosRef.current?.click();
+    console.log('clicked');
+  }
+
+  const handleFileChange = () => {
+    const files = photosRef.current?.files;
+    if (files) {
+      console.log('Selected files:', files);
+      const newFiles = Array.from(files);
+      setphotos(prev => [...prev, ...newFiles]); 
+    }
+  };
+  
+
+
+
   return (
-    <section className='w-full px-28'>
-      <div className='flex justify-start w-full items-start flex-col'>
-      <div className='w-full flex gap-y-6 flex-col justify-start items-start'>
-        <div className='w-full flex gap-x-4 justify-start items-start'>
-          <Image className='w-20 h-20 rounded' alt='vehicleImg' src={VehicleIcon} />
-          <Image className='w-20 h-20 rounded' alt='vehicleImg' src={VehicleIcon} />
-          <Image className='w-20 h-20 rounded' alt='vehicleImg' src={VehicleIcon} />
-          <Image className='w-20 h-20 rounded' alt='vehicleImg' src={VehicleIcon} />
+    <section className='w-full px-28 flex flex-col justify-start items-start'>
+      <div className='flex justify-start gap-y-6 w-full items-start flex-col'>
+        <div className='w-full flex gap-y-6 flex-col justify-start items-start'>
+          <div className='w-full flex gap-x-4 justify-start items-start'>
+            <Image className='w-20 h-20 rounded' alt='vehicleImg' src={VehicleIcon} />
+            <Image className='w-20 h-20 rounded' alt='vehicleImg' src={VehicleIcon} />
+            <Image className='w-20 h-20 rounded' alt='vehicleImg' src={VehicleIcon} />
+            <Image className='w-20 h-20 rounded' alt='vehicleImg' src={VehicleIcon} />
+          </div>
+          <div className='w-fit h-fit'>
+          <input multiple onChange={handleFileChange} className='hidden' type='file' ref={photosRef}/>
+          <Button onClick={uploadPhotos}>Upload image</Button>
+          </div>
         </div>
-        <Button>Upload image</Button>
-      </div>
-      <div className='w-full flex justify-start gap-x-6 items-center h-fit '>
-        <div className='flex justify-start items-start flex-col w-fit h-fit'>
-          <span className='whitespace-nowrap'>Start date</span>
-          <Input />
+        <div className='w-full flex justify-start gap-x-6 items-center h-fit '>
+          <div className='flex justify-start items-start flex-col w-fit h-fit'>
+            <span className='whitespace-nowrap'>Start date</span>
+            <Input />
+          </div>
+          <div className='flex justify-start flex-col items-start w-fit h-fit'>
+            <span className='whitespace-nowrap' >End date</span>
+            <Input />
+          </div>
         </div>
-        <div className='flex justify-start flex-col items-start w-fit h-fit'>
-          <span className='whitespace-nowrap' >End date</span>
-          <Input />
+        <div className='w-full justify-start flex items-start flex-col gap-y-6'>
+          <div className='flex justify-start flex-col items-start w-fit h-fit'>
+            <span className='whitespace-nowrap'>Price</span>
+            <Input />
+          </div>
+          <Button>Register auction</Button>
         </div>
-      </div>
-      <div className='w-full justify-start flex items-start flex-col gap-y-6'>
-        <div className='flex justify-start flex-col items-start w-fit h-fit'>
-          <span className='whitespace-nowrap'>Price</span>
-          <Input />
-        </div>
-        <Button>Register auction</Button>
-      </div>
       </div>
     </section>
   )
