754e516eb59a5bbc6fe2f726efaae0282765decd
yshak <Vyshakn29@gmail.com>
2025-03-04 20:40:09 +0530

    navData working

--- client/app/(dashboadG)/dashboard/view/page.tsx
+++ client/app/(dashboadG)/dashboard/view/page.tsx
@@ -1,13 +1,21 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Infocard } from '../../../../components/viewAuction/Infocard'
 import { UseSelectedAuction } from '@/lib/stateStore/auctionsList'
+import { useRouter } from 'next/navigation'
 
 function page() {
   const {selectedAuction} = UseSelectedAuction();
+  const Router = useRouter();
+
+  useEffect(()=>{
+    if(!selectedAuction){
+      Router.push("/dashboard/home")
+    }
+  },[selectedAuction])
   console.log(selectedAuction)
   return (
-    <section className='w-full'>
+    <section className='w-full pl-20 pr-4 pb-4 h-fit bg-neutral-200 pt-20'>
       {selectedAuction&&<Infocard year={selectedAuction.year} model={selectedAuction.model} kmCovered={selectedAuction.kmCovered} auctionName={selectedAuction.auctionName} brand={selectedAuction.brand}/>}
     </section>
   )
--- client/components/viewAuction/Infocard.tsx
+++ client/components/viewAuction/Infocard.tsx
@@ -11,8 +11,8 @@ interface infoCard {
 function Infocard({auctionName,brand,kmCovered,year,model}:infoCard) {
   return (
     <div className='w-full px-6 py-4 rounded-md bg-white flex justify-start items-start flex-col'>
-      <h4>{auctionName}</h4>
-      <span className='text-neutral-700'>{`${brand} ${model}`}</span>
+      <h4 className='text-neutral-700 font-semibold text-lg'>{auctionName}</h4>
+      <span className='text-neutral-700 font-bold'>{`${brand} ${model}`}</span>
       <div className='flex justify-start items-start gap-x-6'>
         <span className='text-neutral-700'>{kmCovered}</span>
         <span className='text-neutral-700'>{year}</span>
